17db89236c3a6c30c7d4038bd6f74a0ed3f606f2
Sworld <t.sworld@qq.com>
2023-08-25 21:11:46 +0800

    A4

--- exercises/cos_arceos/modules/axdriver/src/filter.rs
+++ exercises/cos_arceos/modules/axdriver/src/filter.rs
@@ -1,3 +1,18 @@
+use core::ops::{Deref,DerefMut};
+
 pub struct NetFilter<T> {
     pub inner: T,
+}
+
+impl<T> Deref for NetFilter<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl<T> DerefMut for NetFilter<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.inner
+    }
 }
